7eaace4564790de746bad0f20ed5c410d1302710
Maxens Soldan <soldan.maxens.com>
2024-04-24 16:18:23 +0200

    Changement du sujet et création du fichier alphabetToFrequence

---
+++ alphabetToFrequence.py
@@ -0,0 +1,16 @@
+def determiner_alphabet_et_frequences(texte):
+    alphabet = {}
+    for caractere in texte:
+        if caractere in alphabet:
+            alphabet[caractere] += 1
+        else:
+            alphabet[caractere] = 1
+    
+    alphabet_trie = sorted(alphabet.items(), key=lambda x: (x[1], ord(x[0])))
+    
+    return alphabet_trie
+
+# Exemple d'utilisation :
+texte_exemple = "ceci est un test avec beaucoup de lettres"
+alphabet_et_frequences = determiner_alphabet_et_frequences(texte_exemple)
+print("Alphabet et fréquences :", alphabet_et_frequences)
